fe510f34b68942172f3ebe4614b13376b54c3705
Michal Rownicki <Michal.Rownicki@unit4.com>
2020-10-09 07:33:29 +0200

    Adding Citites routing

---
+++ CityInfo/CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,39 @@
+﻿using CityInfo.API.Models;
+using System.Collections.Generic;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+
+        public List<CityDto> Citites { get; set; }
+
+        public CitiesDataStore()
+        {
+            Citites = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "New York City",
+                    Description = "The onew ith a big park"
+                },
+
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Antwerp",
+                    Description = "The one with the cathedral that was never finished"
+                },
+
+                new CityDto()
+                {
+                    Id = 3,
+                    Name = "Paris",
+                    Description = "The one with that big tower"
+                }
+            };
+        }
+    }
+}
---
+++ CityInfo/CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,30 @@
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCities()
+        {
+            return Ok(CitiesDataStore.Current.Citites);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCity(int id)
+        {
+            CityDto city = CitiesDataStore.Current.Citites.FirstOrDefault(c => c.Id == id);
+        
+            if(city == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(city);
+        }
+    }
+}
